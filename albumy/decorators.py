e212f44f7cd907c53a628c05956dab2b3d2e0e6d
Tommy0506 <2764393408@qq.com>
2025-08-22 14:22:08 +0800

    extent gap of label line

--- albumy/decorators.py
+++ albumy/decorators.py
@@ -1,14 +1,15 @@
 from functools import wraps
+from markupsafe import Markup
 
-from flask import flash, redirect, Markup, url_for, abort
+from flask import flash, redirect, url_for, abort
 from flask_login import current_user
 
 def confirm_required(func):
     @wraps(func)
     def decorate_confirm(*args, **kwargs):
         if not current_user.confirm_statue:
-            message = Markup("You don't confirm your email"
-                             "Please clink the link resend the email to verify"
+            message = Markup("You don't confirm your email  "
+                             "Please clink the link resend the email to verify  "
                              '<a class="alert-link" href=%s>Resend the email</a>'
                              % url_for('auth.resend_confirm')
                              )
