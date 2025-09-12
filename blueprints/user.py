cd7ff8970220cab4550754fb77b910f0675622c0
Tommy <2764393408@qq.com>
2025-09-12 11:29:13 +0800

    photo siderbar part

--- blueprints/user.py
+++ blueprints/user.py
@@ -5,11 +5,19 @@ from albumy.models import User, Photo
 
 bp = Blueprint('user', __name__)
 
+
+@bp.before_request
+@login_required
+def login_required():
+    pass
+
+
 @bp.route('/index/<username>')
 def index(username):
     user = User.query.filter_by(username=username).first_or_404()
     page = request.args.get('page', 1, type=int)
     per_page = current_app.config["ALBUMY_IMAGE_PER_PAGE"]
-    pagination = Photo.query.with_parent(user).order_by(Photo.timestamp.desc()).paginate(page=page, per_page=per_page)
+    pagination = Photo.query.with_parent(user).order_by(
+        Photo.timestamp.desc()).paginate(page=page, per_page=per_page)
     photos = pagination.items
     return render_template('users/index.html', user=user, photos=photos, pagination=pagination)
