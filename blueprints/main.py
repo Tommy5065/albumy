bb15a22b081d2458878285bb92922a4c47106018
Tommy <2764393408@qq.com>
2025-09-15 11:40:18 +0800

    show tag of photo part

--- blueprints/main.py
+++ blueprints/main.py
@@ -181,7 +181,7 @@ def new_tag(photo_id):
     return redirect(url_for('.show_photo', photo_id=photo.id))
 
 
-@bp.route('/delete/tag/<int:tag_id>/<int:photo_id>', methods=['GET'])
+@bp.route('/delete/tag/<int:tag_id>/<int:photo_id>')
 @confirm_required
 def delete_tag(tag_id, photo_id):
     tag = Tag.query.get_or_404(tag_id)
@@ -197,3 +197,21 @@ def delete_tag(tag_id, photo_id):
         db.session.commit()
     flash('Delete tag successful!', 'info')
     return redirect(url_for('.show_photo', photo_id=photo.id))
+
+
+@bp.route('/tag/<int:tag_id>', defaults={'order_rule': 'by_time'})
+@bp.route('/tag/<int:tag_id>/<order_rule>')
+def show_tag(tag_id, order_rule):
+    tag = Tag.query.get_or_404(tag_id)
+    page = request.args.get("page", 1, type=int)
+    per_page = current_app.config['ALBUMY_IMAGE_PER_PAGE']
+    pagination = Photo.query.with_parent(tag).order_by(
+        Photo.timestamp.desc()).paginate(page=page, per_page=per_page)
+    photos = pagination.items
+    order_rule = 'by_time'
+
+    if order_rule == 'by_collects':
+        # 本来是以收藏者数量为排序，但是没有做collects这个字段，所以暂时用flag字段来代替
+        photos.sort(lambda x: len(x.flag), reversed=True)
+        order_rule = 'by_collects'
+    return render_template('main/tag.html', pagination=pagination, photos=photos, order_rule=order_rule, tag=tag)
